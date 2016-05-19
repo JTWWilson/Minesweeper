b8a388bff73a407456394d3c006f3e6a0d14fbcb
fabcooldog <fabcooldog@Zoobuh.com>
2016-05-19 13:23:36 +0100

    Added bomb-testing and input functions

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,4 +1,4 @@
-def createBoard(x, y, mines):
+def createboard(x, y, mines):
     board = []
     for i in range(0, x):
         column = []
@@ -7,8 +7,37 @@ def createBoard(x, y, mines):
             if [i, j] in mines:
                 column.append('x')
             else:
-                column.append('0')
+                column.append('_')
         board.append(column)
     return board
 
-print(createBoard(5, 5, [[0, 0],[1, 1], [2, 2]]))
+
+def choose(board):
+    x = ''
+    for i in board:
+        for j in i:
+            x += '_ '
+        x += '\n'
+    print(x)
+    selection = [int(input('X axis coord '))]
+    selection.append(int(input('X axis coord ')))
+    count = bombsnear(selection[0], selection[1], board)
+    print(count)
+
+def bombsnear(x, y, board):
+    count = 0
+    xs = [x - 1, x, x + 1]
+    ys = [y - 1, y, y + 1]
+    for i in xs:
+        for j in ys:
+            if board[i][j] == 'x':
+                count += 1
+    return count
+
+def main():
+    board = createboard(5, 5, [[0, 0],[1, 1], [2, 2]])
+    print(board)
+    choose(board)
+
+
+main()
